390208df9a3a2c411f52841e55735c67ba5cd8fa
JosivanCS <50004505+JosivanCS@users.noreply.github.com>
2019-09-01 00:10:53 -0300

    Create q08.c

---
+++ q08.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+main(){
+    int n;
+    printf("Quantidade de elementos sequencia fibonacci: ");
+    scanf("%i", &n);
+
+    int v[n], f1 = 0, f2 = 1, atual;
+    v[0] = 0;
+    v[1] = 1;
+
+    for(int i = 2; i < n; i++){
+        atual = f1 + f2;
+        f1 = f2;
+        f2 = atual;
+        v[i] = atual;
+    }
+
+    for(int i = 0; i < n; i++){
+        printf(" %i", v[i]);
+    }
+
+    printf("\n");
+}
