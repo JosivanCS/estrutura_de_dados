7b44dd91342bd7d2899ea98a4a4fdc9b2a5e92a8
JosivanCS <50004505+JosivanCS@users.noreply.github.com>
2019-08-31 21:56:15 -0300

    Create q05.c

---
+++ q05.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+main(){
+    int n;
+    printf("Digite a quantidade de elementos do vetor A: ");
+    scanf("%i", &n);
+
+    int v1[n], v2[n], valor;
+
+    for(int i = 0; i < n; i++){
+        printf("Digite o elemento %i do vetor A: ", i+1);
+        scanf("%i", &valor);
+        v1[i] = valor;
+        if(v1[i] % 2 == 0){
+            v2[i] = 0;
+        }else{
+            v2[i] = 1;
+        }
+    }
+
+    printf("\n");
+
+    for(int i = 0; i < n; i++){
+        printf("\nvB[%i] = %i", i, v2[i]);
+    }
+
+    printf("\n");
+}
