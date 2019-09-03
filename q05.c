bb994d4af9079fc9719998b40b711b559ee8dafc
JosivanCS <50004505+JosivanCS@users.noreply.github.com>
2019-09-03 10:53:18 -0300

    Update q05.c

--- q05.c
+++ q05.c
@@ -2,13 +2,13 @@
 
 main(){
     int n;
-    printf("Digite a quantidade de elementos do vetor A: ");
+    printf("Digite a quantidade de itens do vetor A: ");
     scanf("%i", &n);
 
     int v1[n], v2[n], valor;
 
     for(int i = 0; i < n; i++){
-        printf("Digite o elemento %i do vetor A: ", i+1);
+        printf("Digite o %i item do vetor A: ", i+1);
         scanf("%i", &valor);
         v1[i] = valor;
         if(v1[i] % 2 == 0){
@@ -24,5 +24,5 @@ main(){
         printf("\nvB[%i] = %i", i, v2[i]);
     }
 
-    printf("\n");
+    printf("\n\n");
 }
