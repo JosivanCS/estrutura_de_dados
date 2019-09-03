18b8046e65e5879c7cb8cf389b7b5e06c9f91421
JosivanCS <50004505+JosivanCS@users.noreply.github.com>
2019-09-03 10:55:06 -0300

    Update q06.c

--- q06.c
+++ q06.c
@@ -1,35 +1,38 @@
 #include<stdio.h>
+#include<stdlib.h>
+
 
 main(){
     int n;
-    printf("Digite a quantidade de elementos do vetor: ");
+    printf("Digite a quantidade de itens do vetor: ");
     scanf("%i", &n);
 
-    int v[n], valor, v1, maior, menor, posi_maior = 0, posi_menor = 0;
+    int v[n], valor, v1, maior, menor, i_maior = 0, i_menor = 0;
 
-    printf("Digite o elemento 1 do vetor: ");
+    printf("Digite o 1 item do vetor: ");
     scanf("%i", &v1);
     v[0] = v1;
     maior = v1;
     menor = v1;
 
     for(int i = 1; i < n; i++){
-        printf("Digite o elemento %i do vetor: ", i+1);
+        printf("Digite o %i item do vetor: ", i+1);
         scanf("%i", &valor);
         v[i] = valor;
         if(v[i] > maior){
             maior = valor;
-            posi_maior = i;
+            i_maior = i;
         }
         if(v[i] < menor){
             menor = valor;
-            posi_menor = i;
+            i_menor = i;
         }
     }
 
     printf("\n");
-    printf("Maior -> v[%i] = %i", posi_maior, maior);
-    printf("\nMenor -> v[%i] = %i", posi_menor, menor);
+    printf("Maior -> v[%i] = %i\nPosicao --> %i", i_maior, maior, i_maior + 1);
+    printf("\n\n");
+    printf("\nMenor -> v[%i] = %i\nPosicao --> %i", i_menor, menor, i_menor + 1);
 
-    printf("\n");
+    printf("\n\n");
 }
