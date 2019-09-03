ef184c5611a717f15a6e27ce571c03b9b83a0356
JosivanCS <50004505+JosivanCS@users.noreply.github.com>
2019-09-03 10:52:31 -0300

    Update q04.c

--- q04.c
+++ q04.c
@@ -1,9 +1,8 @@
 #include<stdio.h>
-#include<string.h>
 #include<math.h>
 
 main(){
-    printf("Digite os 8 elementos do vetor \nOBS: Somente (0 e 1)\n");
+    printf("Digite os 8 itens do binario \nOBS: Somente (0 e 1)\n");
     int v[8], valor, decimal ;
 
     for(int i = 7; i >= 0; i--){
