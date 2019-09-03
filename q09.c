27bc61241be7a548aa81186762d89c7a80d2436b
JosivanCS <50004505+JosivanCS@users.noreply.github.com>
2019-09-03 10:58:18 -0300

    Update q09.c

--- q09.c
+++ q09.c
@@ -1,14 +1,16 @@
 #include<stdio.h>
+#include<stdlib.h>
+
 
 main(){
     int n;
-    printf("Digite a quantidade de elementos do vetor: ");
+    printf("Digite a quantidade de itens do vetor: ");
     scanf("%i", &n);
 
     float v[n], valor, somatorio = 0, media = 0;
 
     for(int i = 0; i < n; i++){
-        printf("Digite o %i elemento do vetor: ", i+1);
+        printf("Digite o %i item do vetor: ", i+1);
         scanf("%f", &valor);
         v[i] = valor;
         somatorio += v[i];
@@ -18,5 +20,5 @@ main(){
 
     printf("\nSomatorio = %.2f", somatorio);
     printf("\nMedia = %.2f", media);
-    printf("\n");
+    printf("\n\n");
 }
